874129a4c6e182541dcfad61f7b257c8955ea0d6
masciugo <masciugo@gmail.com>
2013-04-30 18:00:31 +0200

    clean

--- lib/acts_as_a_relative.rb
+++
@@ -1,7 +0,0 @@
-require File.join(File.expand_path(File.dirname(__FILE__)), 'genealogy/class_methods')
-require File.join(File.expand_path(File.dirname(__FILE__)), 'genealogy/instance_methods')
-# require File.join(File.expand_path(File.dirname(__FILE__)), 'genealogy/exceptions')
-require File.join(File.expand_path(File.dirname(__FILE__)), 'genealogy/has_parents')
-
-module Genealogy
-end
