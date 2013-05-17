592aca501ee8cb9380000d7670ccbd9a208db6d6
masciugo <masciugo@gmail.com>
2013-05-17 14:48:02 +0200

    no message

--- README.md
+++ README.md
@@ -1,12 +1,50 @@
 # Genealogy
 
-Genealogy is a ruby gem library which enhance ActiveRecord models with familiar relationships capabilities in order to build or query genealogies. If records of your model need to be connected and act as they were a family just add two parents column to its table (ie.: *father_id* and *mother_id*) and make your model to *:has_parents*. This macro will provide the two fundamental self-join associations, *father* and *mother*, whose everything depend on.  
+Genealogy is a ruby gem library which enhance ActiveRecord models with familiar relationships capabilities in order to build and query genealogies. If records of your model need to be linked and act as they were individuals of a family just add two parents column to its table (ie.: *father_id* and *mother_id*) and make your model to *:has_parents*. This macro will provide the two fundamental self-join associations, *father* and *mother*, whose everything depend on.  
 Genealogy takes inspiration from the simple [linkage file format](http://www.helsinki.fi/~tsjuntun/autogscan/pedigreefile.html) which represent genealogies in terms of set of trios: *individual_id*, *father_id*, *mother_id*. Basically the only primitive familiar relationships are associations father and mother, all others like grandparents, siblings or offspring are derived. This means that all methods in charge of update the genealogy (adding/removing relatives) will end up to use the fundamental method add/remove_parent to the right records
 
 ## Installation
 
-To apply Ancestry to any ActiveRecord model, follow these simple steps:  
-1.  Install   
-    1.  Add to Gemfile: gem ‘genealogy’   
-    2.  Install required gems: bundle install    
-2. Add ancestry column to your table     
+To apply Genealogy in its simplest form to any ActiveRecord model, follow these simple steps:  
+
+1. Install   
+    1. Add to Gemfile: gem ‘genealogy’   
+    2. Install required gems: bundle install    
+
+2. Add the foreign key parents columns to your table     
+    1. Create migration: `rails g migration add_parents_to_[table] father_id:integer mother_id:integer`       
+    2. Add index to migration    
+    3. Migrate your database: `rake db:migrate`
+
+3. Add `has_parents` to your model
+
+## Usage
+
+Instance methods are two kinds: *modifiers* and *queries*
+
+### Modifiers methods
+
+They change the genealogy updating one ore more target individuals. Here are some examples where target individual match with method's receiver:
+
+* `george.add_father(peter)` will change george's father_id to peter.id
+* `george.add_father(charlie)` will overwrite george's father_id
+* `george.add_mother(gina)` 
+* `george.add_parents(peter,gina)`
+* `george.remove_father` will set to nel george's father_id 
+* `george.remove_parents`
+
+Not always the method's receiver is the instance that get updated:
+
+* `george.add_paternal_grandmother(gina)` will change mother of george's father. No changes for george and gina
+
+Even if it can seem weird try to see it as an intuitive shortcut to quickly build the genealogy.
+Some methods can take a list of individuals:
+
+* `george.add_grandparents(paul,gina,nick,mary)` will change both george's parents
+* `george.add_grandparents(paul,nil,nick,mary)` you can skip unknown relatives
+
+* `julian.add_siblings(gina,rita)` will change gina and rita's parents to the julian's ones
+
+Half parent relationships are admitted
+
+* `luke.add_offspring(julian)` 
--- lib/genealogy/methods.rb
+++ lib/genealogy/methods.rb
@@ -48,52 +48,48 @@ module Genealogy
     # grandparents
     LINEAGE_NAME = { :father => :paternal, :mother => :maternal }
     [:father, :mother].each do |parent|
-      
-      raise LineageGapException, "#{self} doesn't have #{parent}" unless parent
-      
       [:father, :mother].each do |grandparent|
-
-        # add
+        # add one 
         define_method "add_#{LINEAGE_NAME[parent]}_grand#{grandparent}" do |relative|
           raise IncompatibleRelationshipException, "#{self} can't be grand#{grandparent} of itself" if relative == self
+          raise_if_gap_on(parent)
           send(parent).send("add_#{grandparent}",relative)
         end
-
-        # remove
+        # remove one
         define_method "remove_#{LINEAGE_NAME[parent]}_grand#{grandparent}" do
+          raise_if_gap_on(parent)
           send(parent).send("remove_#{grandparent}")
         end
+      end
+    end
 
+    [:father, :mother].each do |parent|
+      # add two by lineage
+      define_method "add_#{LINEAGE_NAME[parent]}_grandparents" do |grandfather,grandmother|
+        raise_if_gap_on(parent)
+        send(parent).send("add_parents",grandfather,grandmother)
+      end
+      # remove two by lineage
+      define_method "remove_#{LINEAGE_NAME[parent]}_grandparents" do
+        raise_if_gap_on(parent)
+        send(parent).send("remove_parents")
       end
     end
 
     # add all
     def add_grandparents(pgf,pgm,mgf,mgm)
       transaction do
-        father.add_parents(pgf,pgm)
-        mother.add_parents(mgf,mgm)
+        add_paternal_grandparents(pgf,pgm)
+        add_maternal_grandparents(mgf,mgm)
       end
     end
 
     # remove all
     def remove_grandparents
       transaction do
-        father.remove_parents
-        mother.remove_parents
+        remove_paternal_grandparents
+        remove_maternal_grandparents
       end
-    end
-
-    LINEAGE_NAME.each do |parent,lineage|
-      define_method "add_#{lineage}_grandparents" do |grandfather,grandmother|
-        raise LineageGapException, "#{self} doesn't have #{parent}" unless send(parent)
-        send(parent).send("add_parents",grandfather,grandmother)
-      end
-
-      define_method "remove_#{lineage}_grandparents" do
-        raise LineageGapException, "#{self} doesn't have #{parent}" unless send(parent)
-        send(parent).send("remove_parents")
-      end
-
     end
 
 
@@ -148,23 +144,23 @@ module Genealogy
 
     # grandparents
     [:father, :mother].each do |parent|
-
-      raise LineageGapException, "#{self} doesn't have #{parent}" unless parent
-      
       [:father, :mother].each do |grandparent|
 
+        # get one
         define_method "#{LINEAGE_NAME[parent]}_grand#{grandparent}" do
-          send(parent).send(grandparent)
+          send(parent) && send(parent).send(grandparent)
         end
 
       end
 
+      # get two by lineage
       define_method "#{LINEAGE_NAME[parent]}_grandparents" do
-        send(parent).parents
+        (send(parent) && send(parent).parents) || []
       end
 
     end
 
+    # get all
     def grandparents
       result = []
       [:father, :mother].each do |parent|
@@ -207,14 +203,18 @@ module Genealogy
 
 
     def siblings
-      if father and mother
-        (father.offspring & mother.offspring) - [self]
+      if father or mother
+        (father.try(:offspring).to_a & mother.try(:offspring).to_a) - [self]
+      else
+        []
       end
     end
 
     def half_siblings
-      if father and mother
-        (father.offspring | mother.offspring) - [self] - siblings
+      if father or mother
+        (father.try(:offspring).to_a | mother.try(:offspring).to_a) - [self] - siblings
+      else
+        []
       end
     end
 
@@ -224,6 +224,14 @@ module Genealogy
 
     def is_male?
       sex == sex_male_value  
+    end
+
+    ##################################################################
+    ## checking methods
+    ##################################################################
+
+    def raise_if_gap_on(relative)
+      raise LineageGapException, "#{self} doesn't have #{relative}" unless send(relative)
     end
 
     module ClassMethods
