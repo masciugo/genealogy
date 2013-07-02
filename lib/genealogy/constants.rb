bf8e1800bdfe48f02e9b80596cf66fac822cac5b
masciugo <masciugo@gmail.com>
2013-07-02 16:02:36 +0200

    roles akas

--- lib/genealogy/constants.rb
+++ lib/genealogy/constants.rb
@@ -4,4 +4,21 @@ module Genealogy
   PARENT2SEX = { :father => :male, :mother => :female }
   SEX2PARENT = PARENT2SEX.invert
   OPPOSITESEX = {:male => :female, :female => :male}
+
+  AKA = {
+    :father => "F",
+    :mother => "M",
+    :paternal_grandfather => "PGF", 
+    :paternal_grandmother => "PGM", 
+    :maternal_grandfather => "MGF", 
+    :maternal_grandmother => "MGM", 
+    :children => "C",
+    :siblings => "S", 
+    :half_siblings => "HS",
+    :paternal_half_siblings => "PHS",
+    :maternal_half_siblings => "MHS", 
+    :grandchildren => "GC", 
+    :uncles_and_aunts => "U&A", 
+    :nieces_and_nephews => "N&N"
+  }
 end
