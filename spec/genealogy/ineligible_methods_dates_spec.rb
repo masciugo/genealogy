23a1d79c41da1d41e7df4b83088b50b6df212931
Cinzia Sala <salcin@gmail.com>
2015-02-25 13:07:08 +0100

    no message

--- spec/genealogy/ineligible_methods_dates_spec.rb
+++ spec/genealogy/ineligible_methods_dates_spec.rb
@@ -40,17 +40,20 @@ describe "*** Ineligible methods ***", :ineligible_dates do
     its(:father_birth_range) { is_expected.to be nil }
     its(:mother_birth_range) { is_expected.to be nil }
   end
-  describe "paul" do
+  describe "paul", :cin do
     subject {paul}
     its(:birth) {is_expected.to eq Date.new(1970,3,3)}
     its(:death) {is_expected.to be nil}
+    its(:ineligible_mothers) {is_expected.to match_array @model.males + [michelle,mia,barbara,beatrix,mary,sue,irene,emily,debby,alison,louise,rosa] }
+
   end
-  describe "titty" do
+  describe "titty", :cin do
     subject {titty}
     its(:birth) {is_expected.to be nil}
     its(:death) {is_expected.to eq Date.new(2010,8,6)}
     its(:ineligible_fathers) {is_expected.to match_array @model.females + [larry,julian,peter,steve,sam,charlie]}
     its(:ineligible_mothers) {is_expected.to match_array @model.males + [titty,beatrix, irene, mary] }
+    its(:ineligible_maternal_grandfathers) {is_expected.to match_array @model.females + [julian,ruben,peter,steve,sam,charlie]}
   end
 
   describe "paso" do
@@ -59,7 +62,7 @@ describe "*** Ineligible methods ***", :ineligible_dates do
     its(:ineligible_mothers) {is_expected.to match_array @model.males + [terry,naomi,maggie,barbara,michelle,mia,sue,beatrix,mary,rosa,louise,irene,emily] }
   end
 
-  describe "mia" , :cin do
+  describe "mia", :cin do
     subject { mia }
     its(:ineligible_children) {is_expected.to match_array [mia,barbara,mark,rud,paul,michelle,maggie,john,paso,irene,manuel,terry,naomi,ned,alison,jack,debby,tommy,emily,marcel,bob,louise,larry,rosa,luis] }
     its(:ineligible_mothers) {is_expected.to match_array @model.males + [mia,michelle,beatrix,mary,sue,maggie,irene,emily,debby,alison,rosa,louise] }
