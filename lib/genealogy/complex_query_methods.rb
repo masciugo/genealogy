e755fae85e496e3817d6e16b374132af14f85aee
Eric Stensland <estensland88@gmail.com>
2015-05-26 18:54:19 -0500

    MVP least common ancestor passes tests

--- lib/genealogy/complex_query_methods.rb
+++ lib/genealogy/complex_query_methods.rb
@@ -3,5 +3,36 @@ module Genealogy
     extend ActiveSupport::Concern
     include Constants
 
+    def least_common_ancestor(other_person)
+      self_parent_ids = [self.id]
+      other_parent_ids = [other_person.id]
+
+      generation_count = 1
+
+      self_ancestor_records = [self]
+      other_ancestor_records = [other_person]
+
+      while self_parent_ids.length > 0 || other_parent_ids.length > 0
+        self_store = gclass.where(id: self_parent_ids)
+        other_store = gclass.where(id: other_parent_ids)
+
+        self_next_gen = self_store.flat_map(&:parents).compact
+        other_next_gen = other_store.flat_map(&:parents).compact
+
+        self_ancestor_records += self_next_gen
+        other_ancestor_records += other_next_gen
+
+        self_parent_ids = self_next_gen.compact.map(&:id)
+        other_parent_ids = other_next_gen.compact.map(&:id)
+
+        if (self_ancestor_records & other_ancestor_records).compact.length > 0
+          return gclass.where(id: (self_ancestor_records & other_ancestor_records).map(&:id))
+        else
+          generation_count += 1
+        end
+      end
+      gclass.where(id: nil)
+    end
+
   end
 end
